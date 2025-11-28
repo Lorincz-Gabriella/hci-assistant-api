40d70dacac85c53872d94473b7c104335eba0248
Lorincz-Gabriella <lorinczgabriella32@gmail.com>
2025-11-28 10:40:20 +0200

    commit

--- src/HCI.AIAssistant.API/Program.cs
+++ src/HCI.AIAssistant.API/Program.cs
@@ -5,6 +5,18 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "CORS",
+    policy =>
+    {
+        policy
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin();
+    });
+});
+
 // Replace appsettings.json values with Key Vault values
 var keyVaultName = builder.Configuration
  [$"AppConfigurations{ConfigurationPath.KeyDelimiter}KeyVaultName"];
@@ -50,6 +62,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
+app.UseCors("CORS");
 app.UseSwagger();
 app.UseSwaggerUI();
 //}
